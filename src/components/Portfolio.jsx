e9f3b1d33f4fe22896f79dcb30785edea7bab0cc
Lino676 <henriquedoslino10@gmail.com>
2025-11-13 20:19:17 -0300

    yes

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -6,14 +6,38 @@ import { useState } from "react";
 function Portfolio() {
     const portfolio = "py-16 px-4 bg-gradient-to-b from-[#ffffff] to-[#bfbfbf]";
     const portfolioH2 = "text-3xl md:text-4xl font-playfair text-black mb-8 text-center";
-    const portfolioDiv = "grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6 max-w-6xl mx-auto";
+    const portfolioDiv = "grid grid-cols-1 sm:grid-cols-2 md:grid-cols-6 gap-6 max-w-6xl mx-auto";
     const itens = [
-        {title: "Foto 1", image:'bg-[#d3c912]'},
-        {title: "Foto 2", image:'bg-[#d3c912]'},
-        {title: "Foto 3", image:'bg-[#d3c912]'},
-        {title: "Foto 4", image:'bg-[#d3c912]'},
-        {title: "Foto 5", image:'bg-[#d3c912]'},
-        {title: "Foto 6", image:'bg-[#d3c912]'},
+        {title: "Foto 1", image: '/imagens/IMG_0006.JPG'},
+        {title: "Foto 2", image: '/imagens/IMG_0013.JPG'},
+        {title: "Foto 3", image: '/imagens/IMG_0018.JPG'},
+        {title: "Foto 4", image: '/imagens/IMG_0030.JPG'},
+        {title: "Foto 5", image: '/imagens/IMG_0032.JPG'},
+        {title: "Foto 6", image: '/imagens/IMG_0039.JPG'},
+        {title: "Foto 7", image: '/imagens/IMG_0044.JPG'},
+        {title: "Foto 8", image: '/imagens/IMG_0063.JPG'},
+        {title: "Foto 9", image: '/imagens/IMG_2671.JPG'},
+        {title: "Foto 10", image: '/imagens/IMG_2678.JPG'},
+        {title: "Foto 11", image: '/imagens/IMG_2735.JPG'},
+        {title: "Foto 12", image: '/imagens/IMG_2747.JPG'},
+        {title: "Foto 13", image: '/imagens/IMG_2748.JPG'},
+        {title: "Foto 14", image: '/imagens/IMG_2750.JPG'},
+        {title: "Foto 15", image: '/imagens/IMG_5932.JPG'},
+        {title: "Foto 16", image: '/imagens/IMG_5933.JPG'},
+        {title: "Foto 17", image: '/imagens/IMG_5934.JPG'},
+        {title: "Foto 18", image: '/imagens/IMG_5935.JPG'},
+        {title: "Foto 19", image: '/imagens/IMG_5936.JPG'},
+        {title: "Foto 20", image: '/imagens/IMG_5937.JPG'},
+        {title: "Foto 21", image: '/imagens/IMG_5938.JPG'},
+        {title: "Foto 22", image: '/imagens/IMG_5939.JPG'},
+        {title: "Foto 23", image: '/imagens/IMG_5940.JPG'},
+        {title: "Foto 24", image: '/imagens/IMG_5941.JPG'},
+        {title: "Foto 25", image: '/imagens/IMG_7969.jpg'},
+        {title: "Foto 26", image: '/imagens/IMG_7976.jpg'},
+        {title: "Foto 27", image: '/imagens/IMG_8005.jpg'},
+        {title: "Foto 28", image: '/imagens/IMG_8012.jpg'},
+        {title: "Foto 29", image: '/imagens/IMG_8017.jpg'},
+        {title: "Foto 30", image: '/imagens/IMG_8030.jpg'}
     ];
     const [selectedCard, setSelectedCard] = useState(null)
 
@@ -35,7 +59,7 @@ function Portfolio() {
                     <PortfolioCard 
                     key={i} 
                     title={card.title} 
-                    image={card.image}
+                    image= {card.image}
                     index={i}
                     onClick={() => setSelectedCard(card)}
                     />
--- src/components/PortfolioCard.jsx
+++ src/components/PortfolioCard.jsx
@@ -1,23 +1,30 @@
 import { motion } from "framer-motion";
-import { ExpandIcon } from "lucide-react";
 
-const PortfolioCard = ({ title, image, index, onClick}) => {
-    return (
-        <motion.div 
-        onClick={onClick}
-        className={`cursor-pointer relative h-48 flex items-center justify-center gap-2 shadow-lg rounded-lg text-white font-semibold text-lg transition-transform duration-300 hover:scale-105 hover:brightness-110 ${image}`}
-        initial = {{opacity: 0, y:10}}
-        whileInView={{opacity: 1, y:0}}
-        viewport={{once: true}}
-        transition={{duration: 0.4, delay: index * 0.1}}
-        >
-            {/* overlay abaixo */}
-            <motion.div className="absolute inset-0 flex items-center justify-center bg-black/75 opacity-0 hover:opacity-100 transition-opacity duration-500 rounded-lg"
-            initial= {{opacity: 0}}
-            whileHover={{opacity: 1}}>
-            <p className="text-white font-poppins text-lg">{title}</p></motion.div>
-            <ExpandIcon size= {20}/>
-        </motion.div>
-    );
+const PortfolioCard = ({ title, image, index, onClick }) => {
+  return (
+    <motion.div
+      onClick={onClick}
+      className="cursor-pointer relative w-full overflow-hidden rounded-lg shadow-lg transition-transform duration-300 hover:scale-105"
+      initial={{ opacity: 0, y: 10 }}
+      whileInView={{ opacity: 1, y: 0 }}
+      viewport={{ once: true }}
+      transition={{ duration: 0.1, delay: index * 0.1 }}
+    >
+      <img
+        src={image}
+        alt={title}
+        className="w-full h-full object-cover"
+      />
+
+      {/* overlay */}
+      <motion.div
+        className="absolute inset-0 flex items-center justify-center bg-black/75 
+        opacity-0 hover:opacity-100 transition-opacity duration-500 rounded-lg"
+      >
+        <p className="text-white font-poppins text-lg">{title}</p>
+      </motion.div>
+    </motion.div>
+  );
 };
+
 export default PortfolioCard;
